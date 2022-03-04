53ca5852093cf518646ede1d8a231db4188edb5a
EkaterinaGrishel <katesobol88@gmail.com>
2022-03-04 19:41:35 +0300

    Нужно больше функций правки

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
-function getRandomIntInclusive(min, max) {
+const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
+
   if (min >= max || min < 0) {
-    return ('Введите корректное значене: min >= 0 и max > min');
+    throw new Error('Введите корректное значене: min >= 0 и max > min');
   }
+
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
-getRandomIntInclusive();
+getRandomInt();
 
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.›
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 
-function getRandomArbitrary(min, max, maxDigits) {
-  if (min >= max || min < 0 || max <= 0 || maxDigits <= 0) {
-    return ('Задан неверный диапазон! Укажите другие числа: min >= 0, max > min, maxDigits <= 0');
+const getRandomFloat = (min, max, maxDigits) => {
+
+  if (min >= max || min < 0 || maxDigits <= 0) {
+    throw new Error('Задан неверный диапазон! Укажите другие числа: min >= 0, max > min, maxDigits <= 0');
   }
 
   const digitsDegree = 10 ** maxDigits;
-  return (((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree).toFixed(maxDigits);
-}
+  const rand = ((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
+  return Math.abs(rand.toFixed(maxDigits));
+};
 
-getRandomArbitrary();
+getRandomFloat();
